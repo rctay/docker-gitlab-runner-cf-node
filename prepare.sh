df2a0f1e232761bdda8279ff046bd89b6d2af364
Ray <rctay89@gmail.com>
2020-05-20 20:24:07 +0800

    Use $@ to execute command with programmatic args

--- prepare.sh
+++ prepare.sh
@@ -5,21 +5,20 @@ fi
 if [ -z "$GITLAB_NEEDS_REGISTRATION" ]; then
   envsubst '${GITLAB_RUNNER_URL},${GITLAB_RUNNER_TOKEN},${GITLAB_RUNNER_NAME}' < /opt/src/config.toml.tmpl > /etc/gitlab-runner/config.toml
 else
-  OTHER_ARGS=""
-  if [ -n "$GITLAB_RUNNER_NAME" ]; then
-    OTHER_ARGS="$OTHER_ARGS --name \"$GITLAB_RUNNER_NAME\"
-  fi
-  if [ -n "$GITLAB_RUNNER_TAGS" ]; then
-    OTHER_ARGS="$OTHER_ARGS --tag-list \"$GITLAB_RUNNER_TAGS\"
-  fi
-  gitlab-runner register \
+  set -- gitlab-runner register \
     --non-interactive \
     --url "$GITLAB_RUNNER_URL" \
     --registration-token "$GITLAB_RUNNER_TOKEN" \
     --executor "shell" \
     --builds-dir "/home/gitlab-runner/builds" \
-    --cache-dir "/home/gitlab-runner/cache" \
-    ${OTHER_ARGS}
+    --cache-dir "/home/gitlab-runner/cache"
+  if [ -n "$GITLAB_RUNNER_NAME" ]; then
+    set -- "$@" --name "$GITLAB_RUNNER_NAME"
+  fi
+  if [ -n "$GITLAB_RUNNER_TAGS" ]; then
+    set -- "$@" --tag-list "$GITLAB_RUNNER_TAGS"
+  fi
+  "$@"
 fi
 echo Starting runner with config:
 cat /etc/gitlab-runner/config.toml
